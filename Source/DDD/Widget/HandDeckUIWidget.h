245650b6966dd152cd3b84e51679c4fd09eea13d
권민규 [kmgmg2391601] <kmgmg2391601@nexon.co.kr>
2026-04-13 20:16:00 +0900

    각 Deck & Hand 별로 다른 UI를 사용할 예정

---
+++ Source/DDD/Widget/HandDeckUIWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HandDeckUIWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DDD_API UHandDeckUIWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
